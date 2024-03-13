73b75b779aa3818b4a92f87834a153f0d656be8f
Rizwan2580 <163341439+Rizwan2580@users.noreply.github.com>
2024-03-14 00:06:18 +0530

    Add files via upload

---
+++ project/index.js
@@ -0,0 +1,20 @@
+const x = document.getElementById("location");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function status() {
+  document.getElementById("status").innerHTML = `Divier Status: Not drunk `;
+  document.getElementById("status1").innerHTML = `Vehicle Status: No problem `;
+
+}
